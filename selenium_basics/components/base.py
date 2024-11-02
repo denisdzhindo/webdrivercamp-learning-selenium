f07cce06f861a78e1a6561d7d8edbacfbcf76bfa
Denis Dzhindo <santacruz@deniss-mbp.lan>
2024-11-02 13:54:04 -0700

    updating main files

--- selenium_basics/components/base.py
+++ selenium_basics/components/base.py
@@ -1,4 +1,14 @@
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
 class Base:
     def __init__(self, driver):
         self.driver = driver
+
+    def click(self, locator):
+        wait = WebDriverWait(self.driver, 10)
+        locator = wait.until(expected_conditions.element_to_be_clickable(locator))
+        locator.click()
+
+
 
